9da26408ce85df165f89887393a8479698ea80e5
Sebastián Gulli <sebastiangulli@outlook.com>
2024-09-20 17:45:39 -0300

    fix icon component, define new classes and apply basic styles to task component

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,9 +1,9 @@
-export const Icon = ({ className, paths }) => {
+export const Icon = ({ className, children }) => {
     return (
         <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
             fill="none" strokeLinecap="round" strokeLinejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            {paths}
+            {children}
         </svg>
     )
 }
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,14 +1,15 @@
 import { Icon } from "./Icon"
 import { toolPaths } from "../icons/toolPaths"
+import '../../public/styles/components/task.css'
 
 export const Task = ({ name, tool }) => {
     return (
-        <div className="flow__container__grid__task">
-            <span className="flow__container__grid__task__name">
+        <div className="flow__container__grid__task task">
+            <span className="flow__container__grid__task__name task-name">
                 {name}
             </span>
-            <div className="flow__container__grid__task__box">
-                <Icon className={'flow__container__grid__task__box__icon'}>
+            <div className="flow__container__grid__task__box task-box centred-element">
+                <Icon className={'flow__container__grid__task__box__icon task-icon'}>
                     {toolPaths[tool]}
                 </Icon>
             </div>
